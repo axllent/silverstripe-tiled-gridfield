a757975ae7f623bd616bd46e44d7099d0b744250
Ralph Slooten <axllent@gmail.com>
2024-11-15 17:36:09 +1300

    Replace deprecated LeftAndMainExtension with Extension

--- src/LeftAndMainTiledGridFieldExt.php
+++ src/LeftAndMainTiledGridFieldExt.php
@@ -2,13 +2,11 @@
 
 namespace Axllent\TiledGridField;
 
-use SilverStripe\Admin\LeftAndMainExtension;
+use SilverStripe\Core\Extension;
 use SilverStripe\View\Requirements;
 
-/*
- * Load extra CSS into CMS
- */
-class LeftAndMainTiledGridFieldExt extends LeftAndMainExtension
+// Load extra CSS into CMS
+class LeftAndMainTiledGridFieldExt extends Extension
 {
     public function init()
     {
